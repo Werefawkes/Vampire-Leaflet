e2321427339c199b95613cf85d940c5f4fa53bc4
Werefox <jack@wrightus.com>
2024-04-27 17:37:10 -0400

    Added GeoJSON data file and imported it to the map

--- public/app.js
+++ public/app.js
@@ -6,3 +6,12 @@ var imageBounds = [[(-imageSize[0] * imageScale) / 2, (-imageSize[1] * imageScal
 var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds);
 
 L.imageOverlay('./london_map_large.jpg', imageBounds).addTo(map);
+
+const loadJson = async () => {
+    var res = await fetch('./london-geojson.json');
+    const json = await res.json();
+    console.log(json.geojson)
+    L.geoJSON(json.geojson).addTo(map)
+}
+
+loadJson()
