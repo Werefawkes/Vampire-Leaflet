6c7f0fed22b1182f94d22fb0bca3f33164be3296
Werefox <jack@wrightus.com>
2024-04-27 16:40:28 -0400

    Added credit for the map used

--- public/app.js
+++ public/app.js
@@ -3,6 +3,6 @@ var imageScale = 0.001;
 
 var imageBounds = [[(-imageSize[0] * imageScale) / 2, (-imageSize[1] * imageScale) / 2], [[(imageSize[0] * imageScale) / 2, (imageSize[1] * imageScale) / 2]]]
 
-var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds).setZoom(10);
+var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds);
 
 L.imageOverlay('./london_map_large.jpg', imageBounds).addTo(map);
