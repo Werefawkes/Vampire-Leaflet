bbc4a0175ab5fb93edafc4b1a1e26dc0054ca50b
Werefox <jack@wrightus.com>
2024-04-29 19:31:25 -0400

    Added Leaflet Geoman

--- public/app.js
+++ public/app.js
@@ -1,17 +1,45 @@
-var imageSize = [14938, 21839];
-var imageScale = 0.001;
+// Set up image
+// var imageBounds = [[0, 0], [-5.461, 7.8127]]
+var imageBounds = [[0, 0], [-5.4, 7.8]]
 
-var imageBounds = [[(-imageSize[0] * imageScale) / 2, (-imageSize[1] * imageScale) / 2], [[(imageSize[0] * imageScale) / 2, (imageSize[1] * imageScale) / 2]]]
+// var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds);
+var imageLondon = L.imageOverlay('./london_map_large.jpg', imageBounds, {
+    attribution: '<a href="https://www.davidrumsey.com/luna/servlet/detail/RUMSEY~8~1~309018~90079521:Composite--Sheets-1-6--Map-of-Londo">(Composite map of) (Sheets 1-6) Map of London, From an actual Survey made in the Years 1824.1825 & 1826. By C. and J. Greenwood. Published by the Proprietors... August 21 1827. </a>'
+});
 
-var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds);
+var map = L.map('map', {
+    layers: [imageLondon],
+    maxBounds: imageBounds
+})
 
-L.imageOverlay('./london_map_large.jpg', imageBounds).addTo(map);
+map.fitBounds(imageBounds)
 
+// Markers
+var test = L.marker([-1, 1])
+
+var markers = L.layerGroup([test])
+
+var overlays = {
+    "Markers": markers
+}
+
+var layerControl = L.control.layers({}, overlays).addTo(map)
+
+// Load GeoJSON from file
 const loadJson = async () => {
     var res = await fetch('./london-geojson.json');
     const json = await res.json();
-    console.log(json.geojson)
+
     L.geoJSON(json.geojson).addTo(map)
 }
 
 loadJson()
+
+map.pm.addControls({
+    position: 'bottomright',
+})
+
+layers = map.pm.getGeomanLayers();
+console.log(layers)
+
+imageLondon.pm.enable()
