b7508f948793cbc3e9cdbca87351ca5856122af6
Werefox <jack@wrightus.com>
2024-04-29 20:46:33 -0400

    Moved geojson to it's own overlay

--- public/app.js
+++ public/app.js
@@ -2,17 +2,20 @@
 // var imageBounds = [[0, 0], [-5.461, 7.8127]]
 var imageBounds = [[0, 0], [-5.4, 7.8]]
 
-// var map = L.map('map').fitBounds(imageBounds).setMaxBounds(imageBounds);
 var imageLondon = L.imageOverlay('./london_map_large.jpg', imageBounds, {
     attribution: '<a href="https://www.davidrumsey.com/luna/servlet/detail/RUMSEY~8~1~309018~90079521:Composite--Sheets-1-6--Map-of-Londo">(Composite map of) (Sheets 1-6) Map of London, From an actual Survey made in the Years 1824.1825 & 1826. By C. and J. Greenwood. Published by the Proprietors... August 21 1827. </a>'
 });
 
-var map = L.map('map', {
-    layers: [imageLondon],
-    maxBounds: imageBounds
-})
+// Load GeoJSON from file
+var domains = L.geoJSON()
+const loadJson = async () => {
+    var res = await fetch('./london-geojson.json');
+    const json = await res.json();
 
-map.fitBounds(imageBounds)
+    domains.addData(json.geojson)
+}
+
+loadJson();
 
 // Markers
 var test = L.marker([-1, 1])
@@ -20,26 +23,34 @@ var test = L.marker([-1, 1])
 var markers = L.layerGroup([test])
 
 var overlays = {
+    "Domains": domains,
     "Markers": markers
 }
 
-var layerControl = L.control.layers({}, overlays).addTo(map)
+var map = L.map('map', {
+    layers: [imageLondon, domains],
+    maxBounds: imageBounds
+})
 
-// Load GeoJSON from file
-const loadJson = async () => {
-    var res = await fetch('./london-geojson.json');
-    const json = await res.json();
+map.fitBounds(imageBounds)
 
-    L.geoJSON(json.geojson).addTo(map)
-}
-
-loadJson()
+var layerControl = L.control.layers({}, overlays, {}).addTo(map)
 
 map.pm.addControls({
     position: 'bottomright',
 })
 
-layers = map.pm.getGeomanLayers();
-console.log(layers)
+var testShape = {
+    vertecies: []
+}
 
-imageLondon.pm.enable()
+map.on("pm:drawstart", ({ workingLayer}) => {
+    testShape.vertecies = [];
+    workingLayer.on("pm:vertexadded", (e) => {
+        testShape.vertecies.push(e.latlng)
+    })
+})
+
+map.on("pm:create", (e) => {
+    console.log(testShape.vertecies)
+})
