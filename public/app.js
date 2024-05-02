5deebd39bdc9deec64ce4034bea0e9e05d33f253
Werefox <jack@wrightus.com>
2024-05-02 15:21:24 -0400

    Got the raw json from the old map instead of exporting it to GeoJson

--- public/app.js
+++ public/app.js
@@ -1,30 +1,31 @@
-// Set up image
-// var imageBounds = [[0, 0], [-5.461, 7.8127]]
 var imageBounds = [[0, 0], [-5.4, 7.8]]
 
 var imageLondon = L.imageOverlay('./london_map_large.jpg', imageBounds, {
     attribution: '<a href="https://www.davidrumsey.com/luna/servlet/detail/RUMSEY~8~1~309018~90079521:Composite--Sheets-1-6--Map-of-Londo">(Composite map of) (Sheets 1-6) Map of London, From an actual Survey made in the Years 1824.1825 & 1826. By C. and J. Greenwood. Published by the Proprietors... August 21 1827. </a>'
 });
 
-// Load GeoJSON from file
+// // Load GeoJSON from file
 var domains = L.geoJSON()
+var markersLayer = L.layerGroup()
+
 const loadJson = async () => {
-    var res = await fetch('./london-geojson.json');
+    var res = await fetch('./data.json');
     const json = await res.json();
 
-    domains.addData(json.geojson)
+    // Markers
+    console.log(json.mapMarkers[0])
+    json.mapMarkers[0].markers.forEach(element => {
+        // Create marker
+        markersLayer.addLayer(L.marker(element.loc))
+    });
 }
-
 loadJson();
 
 // Markers
-var test = L.marker([-1, 1])
-
-var markers = L.layerGroup([test])
 
 var overlays = {
     "Domains": domains,
-    "Markers": markers
+    "Markers": markersLayer
 }
 
 var map = L.map('map', {
@@ -35,22 +36,3 @@ var map = L.map('map', {
 map.fitBounds(imageBounds)
 
 var layerControl = L.control.layers({}, overlays, {}).addTo(map)
-
-map.pm.addControls({
-    position: 'bottomright',
-})
-
-var testShape = {
-    vertecies: []
-}
-
-map.on("pm:drawstart", ({ workingLayer}) => {
-    testShape.vertecies = [];
-    workingLayer.on("pm:vertexadded", (e) => {
-        testShape.vertecies.push(e.latlng)
-    })
-})
-
-map.on("pm:create", (e) => {
-    console.log(testShape.vertecies)
-})
