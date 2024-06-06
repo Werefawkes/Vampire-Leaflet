9a3fd1bd64513bae4958d07b41f72dc2b49a1b7c
Werefox <jack@wrightus.com>
2024-06-05 23:32:33 -0400

    Moved credit to appropriate component and set max zoom on map

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression, LatLngTuple } from 'leaflet';
+// Thanks to this article for helping me get Leaflet to work:
+// https://andresprieto-25116.medium.com/how-to-use-react-leaflet-in-nextjs-with-typescript-surviving-it-21a3379d4d18
+
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from "react-leaflet";
+import { LatLng, LatLngExpression, LatLngTuple } from 'leaflet';
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
@@ -16,6 +19,18 @@ const defaults = {
     zoom: 19,
 }
 
+const position = new LatLng(51.505, -0.09)
+
+const polygon = [
+    [51, 0],
+    [51.01, 0],
+    [51, 1]
+]
+
+function ToLatLng(array: number[][]) {
+    return array.map(([lat, lng]) => (new LatLng(lat, lng)))
+}
+
 const Map = (Map: MapProps) => {
     const { zoom = defaults.zoom, posix } = Map
 
@@ -23,6 +38,7 @@ const Map = (Map: MapProps) => {
         <MapContainer
             center={posix}
             zoom={zoom}
+            maxZoom={16}
             scrollWheelZoom={true}
             style={{ height: "100%", width: "100%" }}
         >
@@ -30,6 +46,8 @@ const Map = (Map: MapProps) => {
                 attribution='&copy; <a href="http://maps.nls.uk/projects/subscription-api/">National Library of Scotland</a>'
                 url="https://api.maptiler.com/tiles/uk-osgb63k1885/{z}/{x}/{y}.png?key=VLTPHr9mfRwVtty3KayI"
             />
+            <Marker position={position} />
+            <Polygon positions={ToLatLng(polygon)} />
         </MapContainer>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import dynamic from "next/dynamic";
 import { useMemo } from "react";
 import Navbar from "./components/Navbar";
 
-// Thanks to this article for helping me get Leaflet to work:
-// https://andresprieto-25116.medium.com/how-to-use-react-leaflet-in-nextjs-with-typescript-surviving-it-21a3379d4d18
 
 export default async function Page() {
     const Map = useMemo(() => dynamic(
