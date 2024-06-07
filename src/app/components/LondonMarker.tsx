d338f9e606b439da1283caccecf91743f051fa23
Werefox <jack@wrightus.com>
2024-06-06 20:06:33 -0400

    Implemented markers with icons

--- src/app/components/LondonMarker.tsx
+++ src/app/components/LondonMarker.tsx
@@ -1,15 +1,18 @@
 import { Marker, Tooltip } from "react-leaflet";
-import { LatLng, LatLngExpression, LatLngTuple } from "leaflet";
+import { icon, LatLng, LatLngExpression, LatLngTuple } from "leaflet";
 
 interface MarkerProps {
 	position: LatLngExpression | LatLngTuple,
-	tooltip: string
+	tooltip: string,
+	category: string
 }
 
-export default function (MarkerP: MarkerProps) {
-	const { position, tooltip } = MarkerP
+
+export default function (props: MarkerProps) {
+	const { position, tooltip, category } = props
 	return (
-		<Marker position={position}>
+		<Marker position={position} icon={icon({iconUrl: category, iconSize: [50, 50]
+		})}>
 			<Tooltip>
 				{tooltip}
 			</Tooltip>
--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -3,8 +3,8 @@
 // Thanks to this article for helping me get Leaflet to work:
 // https://andresprieto-25116.medium.com/how-to-use-react-leaflet-in-nextjs-with-typescript-surviving-it-21a3379d4d18
 
-import { MapContainer, TileLayer, Marker, Popup, Polygon, useMap, useMapEvents, Polyline } from "react-leaflet";
-import { Control, control, LatLng, LatLngExpression, LatLngTuple } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polygon, useMap, useMapEvents, Polyline, LayersControl, LayerGroup } from "react-leaflet";
+import { Control, control, icon, LatLng, LatLngExpression, LatLngTuple } from 'leaflet';
 import J from "../../data.json"
 
 import "leaflet/dist/leaflet.css";
@@ -30,10 +30,6 @@ function ToLatLng(loc: number[]) {
     return new LatLng((loc[0] * scale[0]) + offset[0], (loc[1] * scale[1]) + offset[1] )
 }
 
-function ArrayToLatLng(array: number[][]) {
-    return array.map(([lat, lng]) => (new LatLng(lat, lng)))
-}
-
 const Map = (Map: MapProps) => {
     const { zoom = defaults.zoom, posix } = Map
 
@@ -49,8 +45,13 @@ const Map = (Map: MapProps) => {
                 attribution='&copy; <a href="http://maps.nls.uk/projects/subscription-api/">National Library of Scotland</a>'
                 url="https://api.maptiler.com/tiles/uk-osgb63k1885/{z}/{x}/{y}.png?key=VLTPHr9mfRwVtty3KayI"
             />
-
-            {ImportedData.markers.map((marker) => <LondonMarker key={marker.link} tooltip={marker.link} position={ToLatLng(marker.loc)}/>)}
+            <LayersControl position="topright">
+                <LayersControl.Overlay checked name="Markers">
+                    <LayerGroup>
+                        {ImportedData.markers.map((marker) => <LondonMarker key={marker.link} category={marker.type} tooltip={marker.link} position={ToLatLng(marker.loc)}/>)}
+                    </LayerGroup>
+                </LayersControl.Overlay>
+            </LayersControl>
         </MapContainer>
     )
 }
