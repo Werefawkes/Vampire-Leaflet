ccc0a1d07147b36f03c40bad5f42ffb8cdecd0b8
Werefox <jack@wrightus.com>
2024-07-18 12:04:04 -0400

    Fixed the landing page wiki button, and restyled the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default async function Page() {
     return (
         <div className="m-3 p-3">
             <div className="text-3xl">London Shadows</div>
-            <div className="text-xl">A Vampire: The Masquerade Campaign</div>
-            <hr className="p-2"></hr>
-            <NavButton className="m-1" url="/map">See the map of 1853 London</NavButton>
-            <NavButton className="m-1" url="/wiki" disabled>See info about the characters</NavButton>
+            <div className="text-xl my-1">A Vampire: The Masquerade Campaign</div>
+            <NavButton className="m-1" url="/map">View the map of 1853 London</NavButton>
+            <NavButton className="m-1" url="/wiki">Read info about the characters</NavButton>
+            <hr className="p-2 my-3"></hr>
         </div>
     )
 }
